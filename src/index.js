2819bacc7206a8d5b128293dd02c04bdd86bed8b
4ailabs <159195171+4ailabs@users.noreply.github.com>
2025-05-13 15:53:36 -0600

    Elimina index.js obsoleto

--- src/index.js
+++
@@ -1,6 +0,0 @@
-// Este archivo es solo un redireccionar a index.jsx para mantener compatibilidad
-// Se recomienda usar directamente index.jsx como punto de entrada
-
-import './index.jsx';
-
-console.warn('ADVERTENCIA: Se está usando src/index.js, que está obsoleto. Debería cambiarse a usar src/index.jsx directamente.');
