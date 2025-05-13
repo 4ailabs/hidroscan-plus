f2eed682b6903708859e7d01a450736e6bd2d848
4ailabs <159195171+4ailabs@users.noreply.github.com>
2025-05-13 15:36:21 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Añadido para resolver problemas de rutas en Vercel
+  base: '/',
   server: {
     port: 3000,
     open: true
   },
   build: {
     outDir: 'dist',
-    minify: true,
-    cssMinify: true,
-    sourcemap: false,
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'index.html')
-      }
-    }
+    assetsDir: 'assets',
+    emptyOutDir: true
   }
 })
