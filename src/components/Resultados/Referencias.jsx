4154d9d280f6ea977613ba75daf9eb540517e091
4ailabs <159195171+4ailabs@users.noreply.github.com>
2025-05-13 14:52:37 -0600

    Agregar Referencias.jsx con importación actualizada

--- src/components/Resultados/Referencias.jsx
+++ src/components/Resultados/Referencias.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import { useApp } from '../../context/AppContext';
+import { useApp } from '../../context/AppContext.jsx';
 import { REFERENCIAS_CIENTIFICAS } from '../../data/nutrientes';
 
 // Contenedor principal para las referencias
