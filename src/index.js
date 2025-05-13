913831626ccf76062fa6236fabb770a92f2912fe
4ailabs <159195171+4ailabs@users.noreply.github.com>
2025-05-13 13:26:04 -0600

    Actualizar index.js para usar createElement en lugar de sintaxis JSX

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import App from './components/App';
 import { AppProvider } from './context/AppContext';
 import './index.css';
 
+// Using createElement instead of JSX syntax to avoid parsing issues
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <AppProvider>
-      <App />
-    </AppProvider>
-  </React.StrictMode>,
+  React.createElement(
+    React.StrictMode,
+    null,
+    React.createElement(
+      AppProvider,
+      null,
+      React.createElement(App, null)
+    )
+  )
 );
